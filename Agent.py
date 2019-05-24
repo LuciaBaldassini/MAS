5bb35588bb9d835c52f718568c0ab65081014483
LuciaBaldassini <l.baldassini@student.rug.nl>
2019-05-24 07:49:44 +0200

    added description, utility functions, and created a module called Agent

---
+++ Agent.py
@@ -0,0 +1,17 @@
+import numpy as np 
+hatColour=['blue','red']
+class Agent:
+	def __init__(self, size):
+		self.size = size
+		self.colourHat= np.random.choice(hatColour)
+		self.hats_in_front = []
+        
+	def __repr__(self): # for printing purposes, will be more elaborate once we add the knowledge tree
+		ret = ""
+		ret += "Agent "
+		ret += repr(self.size)
+		ret += " sees the following hats, from tallest to longest:"
+		ret += "\n"
+		for hat in self.hats_in_front:
+			ret += repr(hat)
+		return ret
--- prisoners_and_hats.py
+++ prisoners_and_hats.py
@@ -4,55 +4,53 @@ Created on Sun May 12 14:26:47 2019
 
 @author: danie
 """
+import numpy as np 
+import Agent
 
-class Agent:
-    def __init__(self, size):
-        self.size = size
-        self.hats_in_front = []
-        
-    def __repr__(self): # for printing purposes, will be more elaborate once we add the knowledge tree
-        ret = ""
-        ret += "Agent "
-        ret += repr(self.size)
-        ret += " sees the following hats, from shortest to longest agent:"
-        ret += "\n"
-        for hat in self.hats_in_front:
-            ret += repr(hat)
-        return ret
-    
-    
-if __name__ == '__main__':
-    number_prisoners = int(input("Welcome, how many prisoners would you like? \n"))
-    print("You chose", number_prisoners, "prisoners")
-    
-    agents = []
-    
+
+def assignRandomHat(n):
+	agents = []  
     # obtain and store the colours of the hats
-    print("\nPlease type the colours of the hats of the prisoners from the smallest prisoner to the largest. \nPlease choose only \"red\" or \"blue\"")
-    hats = []
-    i = 0
-    while i < number_prisoners:
-        print("Agent", i, "has the following colour hat:")
-        colour = input("")
-        while (colour != "red" and colour != "blue"):
-            print("Please choose only \"red\" or \"blue\"")
-            print("Agent", i, "has the following colour hat:")
-            colour = input("")
-        new_agent = Agent(i)
-        new_agent.hats_in_front = hats.copy()
-        hats.append(colour) # add after so agents own hat is not added to list
-        agents.append(new_agent)
-        i += 1
+	#print("\nPlease type the colours of the hats of the prisoners from the smallest prisoner to the largest. \nPlease choose only \"red\" or \"blue\"")
+	hats = []
+	i = 1
+	while i < number_prisoners+1:
+		new_agent = Agent.Agent(i)
+		new_agent.hats_in_front = hats.copy()
+		hats.append(new_agent.colourHat)
+		agents.append(new_agent)
+		#print("Agent", i, "has the following colour hat:")
+		#colour = input("")
+		#while (colour != "red" and colour != "blue"):
+		#	print("Please choose only \"red\" or \"blue\"")
+		#	print("Agent", i, "has the following colour hat:")
+		#	colour = input("")
+		#new_agent = Agent(i)
+		#new_agent.hats_in_front = hats.copy()
+		#hats.append(colour) # add after so agents own hat is not added to list
+		#agents.append(new_agent)
+		i += 1
+	showHatDistribution(agents)
+		
     
-    print("\n")
-    # what all agents see
-    for agent in agents:
-        print(agent)
+def showHatDistribution(agents):
+	for agent in reversed(agents):
+		print(agent)
+	   
+if __name__ == '__main__':
+	descriptionChoice= str(input("Welcome, would you like to read the description of this riddle? [yes/no] \n"))
+	if(descriptionChoice=="yes"):
+		with open('description.txt', 'r') as d:
+			print(d.read())
+	number_prisoners = int(input("How many prisoners would you like? \n"))
+	print("You chose", number_prisoners, "prisoners")   
+	assignRandomHat(number_prisoners)
+    
+
     
     
         
-        
-        
+  
         
         
         
